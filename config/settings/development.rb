aa54b151dfb4bf52d4133a0f73818eddd29f720c
cfeliciano <c.feliciano2009@gmail.com>
2013-10-05 03:06:51 -0400

    Added Environoment variables to development

--- config/settings/development.rb
+++ config/settings/development.rb
@@ -4,5 +4,8 @@
 SimpleConfig.for :application do
   group :facebook do
     set :cache_expiry_time, 1.minute
+    set :namespace, ENV['FACEBOOK_NAMESPACE']
+    set :app_id, ENV['FACEBOOK_APP_ID']
+    set :secret, ENV['FACEBOOK_SECRET']
   end
 end
